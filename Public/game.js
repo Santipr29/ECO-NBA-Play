029ffc44b8993f625f545eccd354707ad85d7a5f
Santiago Perez <santipr2004@gmail.com>
2023-10-16 10:54:27 -0500

    Feat: ball drag

--- Public/game.js
+++ Public/game.js
@@ -1,14 +1,16 @@
-let ball, post, basket;
+let ballImg, post, basket;
 
+let startDrag;
 
 function preload(){
-    ball = loadImage('img/balon.png');
+    ballImg = loadImage('img/balon.png');
     post = loadImage('img/palo.png');
     basket = loadImage('img/aro.png');
 }
 
 function setup() {
     createCanvas(600, 658);
+    ball = new Ball(230,520);
 }
   
 function draw() {
@@ -16,7 +18,38 @@ function draw() {
 
     image(post, 150, 50, 250, 700);
     image(basket, 230, 180, 100, 100);
-    image(ball,230,520, 80, 80);
-    
+
+    ball.show();
+    ball.update();
 }
 
+class Ball{
+    constructor(x,y){
+        this.pos = createVector(x,y)
+        this.vel = createVector(0,0)
+    }
+
+    show(){
+        image(ballImg, this.pos.x , this.pos.y, 80, 80);
+    }
+
+    update(){
+        this.pos.add(this.vel);
+    }
+
+    launch(direction){
+        const dir = direction.copy().normalize();
+
+        this.vel = dir.mult(direction.mag()*0.1)
+    }
+}
+
+function mousePressed(){
+    startDrag = createVector(mouseX, mouseY);
+}
+
+function mouseReleased(){
+    const endDrag = createVector(mouseX, mouseY);
+    const dragVector = p5.Vector.sub(endDrag, startDrag);
+    ball.launch(dragVector)
+}
