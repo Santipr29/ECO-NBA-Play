aed50e77e50daa064969cbb4f87db22d321f5bd2
juan esteban ramirez perdomo <juanestebanrp123@gmail.com>
2023-10-16 20:59:51 -0500

    feat:score

--- Public/game.js
+++ Public/game.js
@@ -1,78 +1,119 @@
 let ballImg, post, basket;
-
 let startDrag;
+let score = 0;
+let ballFalling = false;
+let enteredBasket = false;
+let ballSize = 50; 
+let prevPosY = 0;
 
-function preload(){
-    ballImg = loadImage('img/balon.png');
-    post = loadImage('img/palo.png');
-    basket = loadImage('img/aro.png');
+function preload() {
+  ballImg = loadImage('img/balon.png');
+  post = loadImage('img/palo.png');
+  basket = loadImage('img/aro.png');
 }
 
 function setup() {
-    createCanvas(600, 658);
-    ball = new Ball(230,520);
+  createCanvas(600, 658);
+  ball = new Ball(230, 520);
 }
-  
+
 function draw() {
-    background(220);
+  background(220);
 
-    image(post, 150, 50, 250, 700);
-    image(basket, 230, 180, 100, 100);
+  image(post, 150, 50, 250, 700);
+  image(basket, 230, 180, 100, 100);
 
-    ball.show();
-    ball.update();
+  // Ajusta las coordenadas del área de juego en el eje X
+  let minX = 200; // Cambia esto según tus necesidades
+  let maxX = 300; // Cambia esto según tus necesidades
+
+  textSize(24);
+  fill(0);
+  text("Puntos: " + score, 10, 30);
+
+  ball.show();
+  ball.update();
+  checkBasketEntry(minX, maxX);
 }
 
-class Ball{
-    constructor(x,y){
-        this.startPos = createVector(x,y);
-        this.pos = this.startPos.copy();
+class Ball {
+  constructor(x, y) {
+    this.startPos = createVector(x, y);
+    this.pos = this.startPos.copy();
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.force = 0;
+  }
 
-        //this.pos = createVector(x,y);
-        this.vel = createVector(0,0);
-        this.acc = createVector(0,0);
-        this.force = 0;
+  show() {
+    image(ballImg, this.pos.x, this.pos.y, ballSize, ballSize);
+  }
+
+  update() {
+    const gravity = createVector(0, 0.2);
+    this.acc.add(gravity);
+
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+
+    this.acc.mult(0);
+
+    if (this.pos.x < 0 || this.pos.x > width || this.pos.y < 0) {
+      this.reset();
     }
 
-    show(){
-        image(ballImg, this.pos.x , this.pos.y, 80, 80);
+    if (this.pos.y > prevPosY) {
+      ballFalling = true;
+    } else {
+      ballFalling = false;
     }
 
-    update(){
-        const gravity = createVector(0, 0.2);
-        this.acc.add(gravity);
+    prevPosY = this.pos.y;
+  }
 
-        this.vel.add(this.acc);
-        this.pos.add(this.vel);
+  reset() {
+    this.pos = this.startPos.copy();
+    this.vel.mult(0);
+    this.acc.mult(0);
+  }
 
-        this.acc.mult(0);
+  launch(direction) {
+    const dir = direction.copy().normalize();
 
-        if(this.pos.x < 0 || this.pos.x > width || this.pos.y < 0){
-            this.reset();
-        }
-    }
+    this.vel = dir.mult(direction.mag() * 0.1);
 
-    reset(){
-        this.pos = this.startPos.copy();
-        this.vel.mult(0);
-        this.acc.mult(0);
-    }
-
-    launch(direction){
-        const dir = direction.copy().normalize();
-
-        this.vel = dir.mult(direction.mag()*0.1)
-
-        this.acc.mult(0)
-    }
+    this.acc.mult(0);
+  }
 }
 
-function mousePressed(){
-    startDrag = createVector(mouseX, mouseY);
+function mousePressed() {
+  startDrag = createVector(mouseX, mouseY);
 }
 
-function mouseReleased(){
+function mouseReleased() {
+  if (
+    ball.pos.x > 150 &&
+    ball.pos.x < 400 &&
+    ball.pos.y > 50 &&
+    ball.pos.y < 750
+  ) {
     const endDrag = createVector(mouseX, mouseY);
     const dragVector = p5.Vector.sub(endDrag, startDrag);
-    ball.launch(dragVector)
+    ball.launch(dragVector);
+    enteredBasket = false;
+  }
+}
+
+function checkBasketEntry(minX, maxX) {
+  if (!enteredBasket && ballFalling) {
+    if (
+      ball.pos.x + ballSize / 2 > minX &&
+      ball.pos.x - ballSize / 2 < maxX &&
+      ball.pos.y + ballSize / 2 > 180 &&
+      ball.pos.y - ballSize / 2 < 280
+    ) {
+      score++;
+      enteredBasket = true;
+    }
+  }
 }
