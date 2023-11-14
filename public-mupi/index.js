5c3ca40b4beb8eba9619e3314ba0d4f62d8997a3
Santiago <santipr2004@gmail.com>
2023-11-14 08:15:35 -0500

    feat: Mupi QR screen complete

--- public-mupi/index.js
+++ public-mupi/index.js
@@ -1,18 +1,22 @@
 // Importar la clase MupiGameScreen desde el archivo 'game.js'
 import { MupiGameScreen } from './screens/game.js';
+import {MupiQRScreen} from './screens/qr.js'
 
 const app = (p5) => {
   // Crear una instancia de la clase MupiGameScreen
   const mupiGameScreen = new MupiGameScreen(p5);
+  const mupiQRScreen = new MupiQRScreen(p5)
 
   // Configuración inicial de p5.js
   p5.setup = () => {
-    mupiGameScreen.setup();
+    //mupiGameScreen.setup();
+    mupiQRScreen.setup();
   };
 
   // Función de dibujo de p5.js
   p5.draw = () => {
-    mupiGameScreen.draw();
+    //mupiGameScreen.draw();
+    mupiQRScreen.draw();
   };
 };
 
