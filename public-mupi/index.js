d4d369e16461eee17a905e6f328df2efc4089f38
Santiago Perez <santipr2004@gmail.com>
2023-11-14 19:55:52 -0500

    feat: corrections

--- public-mupi/index.js
+++ public-mupi/index.js
@@ -2,25 +2,29 @@
 import { MupiGameScreen } from './screens/game.js';
 import {MupiQRScreen} from './screens/qr.js'
 import { MupiMaincreen } from './screens/main.js';
+import { MupiScoreScreen } from './screens/scores.js';
 
 const app = (p5) => {
   // Crear una instancia de la clase MupiGameScreen
   const mupiGameScreen = new MupiGameScreen(p5);
   const mupiQRScreen = new MupiQRScreen(p5);
   const mupiMainScreen = new MupiMaincreen(p5);
+  const mupiScoreScreen = new MupiScoreScreen(p5)
 
   // Configuración inicial de p5.js
   p5.setup = () => {
     //mupiGameScreen.setup();
     //mupiQRScreen.setup();
-    mupiMainScreen.setup();
+    //mupiMainScreen.setup();
+    mupiScoreScreen.setup();
   };
 
   // Función de dibujo de p5.js
   p5.draw = () => {
     //mupiGameScreen.draw();
     //mupiQRScreen.draw();
-    mupiMainScreen.draw();
+    //mupiMainScreen.draw();
+    mupiScoreScreen.draw();
   };
 };
 
