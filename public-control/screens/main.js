74851996615eda60a79dd1d33a939f3f421fe2fb
Santiago Perez <santipr2004@gmail.com>
2023-11-19 08:37:05 -0500

    Feat: main page style finish

--- public-control/screens/main.js
+++ public-control/screens/main.js
@@ -1,71 +1,40 @@
 export class CellPhoneMainScreen {
-    constructor(p5) {
-        this.p5 = p5;
-    
-        this.back;
-        this.logo;
-        this.players;
+  constructor(p5) {
+      this.p5 = p5;
+  
+      this.back;
+      this.logo;
+      this.players;
 
-        this.logIn = new Button(p5, 'Log In', 'red', 20, 270, 350, 50);
-        this.signUp = new Button(p5, 'Sign Up', 'blue', 20, 340, 350, 50);
-    
-        this.socket = io.connect('http://localhost:5500', { path: '/real-time' });
-    
-        // Cargar imágenes
-        this.back = this.p5.loadImage('img/backcel.png');
-        this.logo = this.p5.loadImage('img/logocel.png');
-        this.players = this.p5.loadImage('img/mainimg2.png');
-        
-      }
-
-      setup(){
-        this.p5.createCanvas(390, 844);
-      }
-
-      draw() {
-        this.p5.background(220);
-        this.p5.image(this.back,0,0);
-        this.p5.image(this.logo, 90, 30);
-        this.p5.image(this.players, 0,420);
-        this.logIn.draw();
-        this.signUp.draw();
-      }
-
-      mouseClicked() {
-        if (this.logIn.isMouseOver()) {
+      this.logIn = p5.createButton('Log In').position(20, 270).size(350, 50);
+      this.logIn.style('background-color', '#006AB7').style('border-radius', '10px').style('border', '2px solid #0F62FE').style('font-size', '16px').style('color', 'white').style('font-weight', 'bold');
+      this.signUp = p5.createButton('Sign Up').position(20, 340).size(350, 50);
+      this.signUp.style('background-color', '#D71E54').style('border-radius', '10px').style('border', '2px solid #F43F74').style('font-size', '16px').style('color', 'white').style('font-weight', 'bold');
+  
+      this.socket = io.connect('http://localhost:5500', { path: '/real-time' });
+  
+      // Cargar imágenes
+      this.back = this.p5.loadImage('img/backcel.png');
+      this.logo = this.p5.loadImage('img/logocel.png');
+      this.players = this.p5.loadImage('img/mainimg2.png');
+      
+      this.logIn.mousePressed(() => {
           console.log('Log In clicked!');
-        } else if (this.signUp.isMouseOver()) {
+      });
+
+      this.signUp.mousePressed(() => {
           console.log('Sign Up clicked!');
-        }
-      }
-}
+      });
+    }
 
-class Button {
-  constructor(p5, label, color, x, y, width, height) {
-    this.p5 = p5;
-    this.label = label;
-    this.color = color;
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-  }
+    setup(){
+      this.p5.createCanvas(390, 844);
+    }
 
-  draw() {
-    this.p5.fill(this.color);
-    this.p5.rect(this.x, this.y, this.width, this.height);
-    this.p5.fill(255);
-    this.p5.textSize(18);
-    this.p5.textAlign(this.p5.CENTER, this.p5.CENTER);
-    this.p5.text(this.label, this.x + this.width / 2, this.y + this.height / 2);
-  }
-
-  isMouseOver() {
-    return (
-      this.p5.mouseX > this.x &&
-      this.p5.mouseX < this.x + this.width &&
-      this.p5.mouseY > this.y &&
-      this.p5.mouseY < this.y + this.height
-    );
-  }
+    draw() {
+      this.p5.background(220);
+      this.p5.image(this.back,0,0);
+      this.p5.image(this.logo, 90, 30);
+      this.p5.image(this.players, 0,420);
+    }
 }
--- public-control/screens/signup.js
+++ public-control/screens/signup.js
@@ -1,190 +1,65 @@
-export class CellPhoneSignUpScreen {
-    constructor(p5) {
-        this.p5 = p5;
-    
-        this.back;
-        this.logo;
+// export class CellPhoneSignUpScreen {
+//   constructor(p5) {
+//       this.p5 = p5;
+  
+//       this.back;
+//       this.logo;
+  
+//       this.socket = io.connect('http://localhost:5500', { path: '/real-time' });
+  
+//       // Crear campos de entrada y botón usando createInput y createButton
+//       this.firstNameInput = p5.createInput().position(24, 340).size(150, 30);
+//       this.lastNameInput = p5.createInput().position(215, 340).size(150, 30);
+//       this.emailInput = p5.createInput().position(24, 440).size(345, 30);
+//       this.passwordInput = p5.createInput().position(24, 547).size(345, 30).attribute('type', 'password');
+//       this.genderSelect = p5.createSelect().position(24, 665).size(345, 30);
+//       this.signUpButton = p5.createButton('Sign Up').position(20, 720).size(350, 40).style('background-color', 'blue');
 
-    
-        this.socket = io.connect('http://localhost:5500', { path: '/real-time' });
-    
-        this.firstNameInput = new InputField(p5, 'First Name', 24, 340, 150, 30);
-        this.lastNameInput = new InputField(p5, 'Last Name', 215, 340, 150, 30);
-        this.emailInput = new InputField(p5, 'Email', 24, 440, 345, 30, 'email');
-        this.passwordInput = new InputField(p5, 'Password', 24, 547, 345, 30, 'password');
-        this.genderSelect = new SelectField(p5, 'Gender', 24, 665, 345, 30, ['Male', 'Female']);
-        this.signUpButton = new Button(p5, 'Sign Up', 'blue', 20, 720, 350, 40);
+//       // Agregar opciones al campo de selección
+//       this.genderSelect.option('Male');
+//       this.genderSelect.option('Female');
 
-        // Cargar imágenes
-        this.back = this.p5.loadImage('img/backcel.png');
-        this.logo = this.p5.loadImage('img/logocel.png');
-        
-      }
+//       // Cargar imágenes
+//       this.back = this.p5.loadImage('img/backcel.png');
+//       this.logo = this.p5.loadImage('img/logocel.png');
+//   }
 
-      setup(){
-        this.p5.createCanvas(390, 844);
-      }
+//   setup() {
+//       this.p5.createCanvas(390, 844);
+//   }
 
-      draw() {
-        this.p5.background(220);
+//   draw() {
+//       this.p5.background(220);
 
-        this.p5.image(this.back,0,0);
-        this.p5.image(this.logo, 90, 20);
+//       this.p5.image(this.back, 0, 0);
+//       this.p5.image(this.logo, 90, 20);
 
-        this.p5.fill(255);
-        this.p5.rect(0, 200, 390, 650); // Recuadro blanco
+//       this.p5.fill(255);
+//       this.p5.rect(0, 200, 390, 650); // Recuadro blanco
 
-        this.p5.fill(0);
-        this.p5.textSize(32);
-        this.p5.textAlign(this.p5.CENTER, this.p5.CENTER);
-    
-        // Texto principal
-        this.p5.text("Sign Up", 80, 260);
-    
-        // Subtitulos
-        this.p5.textSize(14);
-        this.p5.text("First Name", 60, 320);
-        this.p5.text("Last Name", 250, 320);
-        this.p5.text("Email", 45, 420);
-        this.p5.text("Password", 58, 525);
-        this.p5.text("Gender", 50, 640);
+//       this.p5.fill(0);
+//       this.p5.textSize(32);
+//       this.p5.textAlign(this.p5.CENTER, this.p5.CENTER);
+  
+//       // Texto principal
+//       this.p5.text("Sign Up", 80, 260);
+  
+//       // Subtítulos
+//       this.p5.textSize(14);
+//       this.p5.text("First Name", 60, 320);
+//       this.p5.text("Last Name", 250, 320);
+//       this.p5.text("Email", 45, 420);
+//       this.p5.text("Password", 58, 525);
+//       this.p5.text("Gender", 50, 640);
 
-        this.p5.textSize(16);
-        this.firstNameInput.draw();
-        this.lastNameInput.draw();
-        this.emailInput.draw();
-        this.passwordInput.draw();
-        this.genderSelect.draw();
-        this.signUpButton.draw();
-      }
-}
+//       this.p5.textSize(16);
 
-class InputField {
-    constructor(p5, label, x, y, width, height) {
-        this.p5 = p5;
-        this.x = x;
-        this.y = y;
-        this.label = label;
-        this.value = '';
-        this.width = width;
-        this.height = height;
-        this.isFocused = false;
-      }
-  
-    draw() {
-      // Dibuja el campo de entrada
-      this.p5.fill(255);
-      this.p5.stroke(0);
-      this.p5.rect(this.x, this.y, this.width, this.height);
-  
-      // Dibuja la etiqueta
-      this.p5.fill(0);
-      this.p5.textAlign(this.p5.LEFT, this.p5.CENTER);
-      this.p5.text(this.label, this.x, this.y + this.height / 2);
-  
-      // Dibuja el valor ingresado
-      this.p5.fill(0);
-      this.p5.textAlign(this.p5.LEFT, this.p5.CENTER);
-      this.p5.text(this.value, this.x + 10, this.y + this.height / 2);
-  
-      // Si el campo está enfocado, muestra el cursor intermitente
-      if (this.isFocused && frameCount % 60 < 30) {
-        this.p5.line(this.x + 10 + textWidth(this.value), this.y + 5, this.x + 10 + textWidth(this.value), this.y + this.height - 5);
-      }
-    }
-  
-    handleInput() {
-      // Lógica para manejar la entrada de texto (teclado/táctil)
-      if (this.isFocused) {
-        if (keyIsPressed && keyCode !== BACKSPACE && keyCode !== ENTER) {
-          this.value += key;
-        } else if (keyCode === BACKSPACE && this.value.length > 0) {
-          this.value = this.value.slice(0, -1);
-        }
-      }
-    }
-  
-    handleClick(mx, my) {
-      // Verifica si se hizo clic en el campo
-      if (mx > this.x && mx < this.x + this.width && my > this.y && my < this.y + this.height) {
-        this.isFocused = true;
-      } else {
-        this.isFocused = false;
-      }
-    }
-}
-  
-class SelectField {
-    constructor(p5, label, x, y, width, height, options) {
-        this.p5 = p5;
-        this.x = x;
-        this.y = y;
-        this.label = label;
-        this.options = options;
-        this.selectedOption = '';
-        this.width = width;
-        this.height = height;
-        this.isExpanded = false;
-      }
-  
-    draw() {
-      // Dibuja el campo de selección
-      this.p5.fill(255);
-      this.p5.stroke(0);
-      this.p5.rect(this.x, this.y, this.width, this.height);
-  
-      // Dibuja la etiqueta
-      this.p5.fill(0);
-      this.p5.textAlign(this.p5.LEFT, this.p5.CENTER);
-      this.p5.text(this.label, this.x, this.y + this.height / 2);
-  
-      // Dibuja la opción seleccionada
-      this.p5.fill(0);
-      this.p5.textAlign(this.p5.LEFT, this.p5.CENTER);
-      this.p5.text(this.selectedOption, this.x + 10, this.y + this.height / 2);
-    }
-  
-    handleClick(mx, my) {
-      // Verifica si se hizo clic en el campo de selección
-      if (mx > this.x && mx < this.x + this.width && my > this.y && my < this.y + this.height) {
-        this.isExpanded = !this.isExpanded;
-      } else {
-        this.isExpanded = false;
-      }
-    }
-  
-    handleSelection(mx, my) {
-      // Verifica si se hizo clic en una opción cuando el campo de selección está expandido
-      if (this.isExpanded) {
-        for (let i = 0; i < this.options.length; i++) {
-          const optionY = this.y + this.height + i * this.height;
-          if (mx > this.x && mx < this.x + this.width && my > optionY && my < optionY + this.height) {
-            this.selectedOption = this.options[i];
-            this.isExpanded = false;
-            break;
-          }
-        }
-      }
-    }
-}
-
-class Button {
-    constructor(p5, label, color, x, y, width, height) {
-      this.p5 = p5;
-      this.label = label;
-      this.color = color;
-      this.x = x;
-      this.y = y;
-      this.width = width;
-      this.height = height;
-    }
-  
-    draw() {
-      this.p5.fill(this.color);
-      this.p5.rect(this.x, this.y, this.width, this.height);
-      this.p5.fill(255);
-      this.p5.textSize(18);
-      this.p5.textAlign(this.p5.CENTER, this.p5.CENTER);
-      this.p5.text(this.label, this.x + this.width / 2, this.y + this.height / 2);
-    }
-}
+//       // Mostrar campos de entrada y botón
+//       this.firstNameInput.show();
+//       this.lastNameInput.show();
+//       this.emailInput.show();
+//       this.passwordInput.show();
+//       this.genderSelect.show();
+//       this.signUpButton.show();
+//   }
+// }
