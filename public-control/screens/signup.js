23bc0d607b231aacb28b9f8c0350f0062b7d1a9e
Santiago Perez <santipr2004@gmail.com>
2023-11-19 10:06:12 -0500

    Feat: sign up screen finish

--- public-control/screens/signup.js
+++ public-control/screens/signup.js
@@ -1,65 +1,92 @@
-// export class CellPhoneSignUpScreen {
-//   constructor(p5) {
-//       this.p5 = p5;
+export class CellPhoneSignUpScreen {
+  constructor(p5) {
+      this.p5 = p5;
   
-//       this.back;
-//       this.logo;
+      this.back;
+      this.logo;
   
-//       this.socket = io.connect('http://localhost:5500', { path: '/real-time' });
+      this.socket = io.connect('http://localhost:5500', { path: '/real-time' });
   
-//       // Crear campos de entrada y botón usando createInput y createButton
-//       this.firstNameInput = p5.createInput().position(24, 340).size(150, 30);
-//       this.lastNameInput = p5.createInput().position(215, 340).size(150, 30);
-//       this.emailInput = p5.createInput().position(24, 440).size(345, 30);
-//       this.passwordInput = p5.createInput().position(24, 547).size(345, 30).attribute('type', 'password');
-//       this.genderSelect = p5.createSelect().position(24, 665).size(345, 30);
-//       this.signUpButton = p5.createButton('Sign Up').position(20, 720).size(350, 40).style('background-color', 'blue');
+      // Crear campos de entrada y botón usando createInput y createButton
+      this.firstNameInput = p5.createInput().position(24, 340).size(150, 30);
+      this.firstNameInput.style('background-color', '#F2F4F8').style('border-radius', '10px').style('border', '2px solid #C1C7CD').style('font-size', '16px').style('color', '#697077').style('font-weight', 'regular');
 
-//       // Agregar opciones al campo de selección
-//       this.genderSelect.option('Male');
-//       this.genderSelect.option('Female');
+      this.lastNameInput = p5.createInput().position(215, 340).size(150, 30);
+      this.lastNameInput.style('background-color', '#F2F4F8').style('border-radius', '10px').style('border', '2px solid #C1C7CD').style('font-size', '16px').style('color', '#697077').style('font-weight', 'regular');
 
-//       // Cargar imágenes
-//       this.back = this.p5.loadImage('img/backcel.png');
-//       this.logo = this.p5.loadImage('img/logocel.png');
-//   }
+      this.emailInput = p5.createInput().position(24, 440).size(345, 30).attribute('type', 'email');
+      this.emailInput.style('background-color', '#F2F4F8').style('border-radius', '10px').style('border', '2px solid #C1C7CD').style('font-size', '16px').style('color', '#697077').style('font-weight', 'regular');
 
-//   setup() {
-//       this.p5.createCanvas(390, 844);
-//   }
+      this.passwordInput = p5.createInput().position(24, 547).size(345, 30).attribute('type', 'password');
+      this.passwordInput.style('background-color', '#F2F4F8').style('border-radius', '10px').style('border', '2px solid #C1C7CD').style('font-size', '16px').style('color', '#697077').style('font-weight', 'regular');
 
-//   draw() {
-//       this.p5.background(220);
+      this.genderSelect = p5.createSelect().position(24, 665).size(345, 30);
+      this.genderSelect.style('background-color', '#F2F4F8').style('border-radius', '10px').style('border', '2px solid #C1C7CD').style('font-size', '16px').style('color', '#697077').style('font-weight', 'regular');
 
-//       this.p5.image(this.back, 0, 0);
-//       this.p5.image(this.logo, 90, 20);
+      this.signUpButton = p5.createButton('Sign Up').position(20, 720).size(350, 40);
+      this.signUpButton.style('background-color', '#D71E54').style('border-radius', '10px').style('border', '2px solid #F43F74').style('font-size', '16px').style('color', 'white').style('font-weight', 'bold');
 
-//       this.p5.fill(255);
-//       this.p5.rect(0, 200, 390, 650); // Recuadro blanco
+      this.account = p5.createButton('Already have an account?').position(19, 800);
+      this.account.style('background-color', 'transparent').style('border', '1px solid transparent').style('font-size', '14px').style('color', '#001D6C').style('font-weight', 'regular');
 
-//       this.p5.fill(0);
-//       this.p5.textSize(32);
-//       this.p5.textAlign(this.p5.CENTER, this.p5.CENTER);
+      // Agregar opciones al campo de selección
+      this.genderSelect.option('Male');
+      this.genderSelect.option('Female');
+
+      // Cargar imágenes
+      this.back = this.p5.loadImage('img/backcel.png');
+      this.logo = this.p5.loadImage('img/logocel.png');
+
+      this.signUpButton.mousePressed(() => {
+        console.log('Sign up clicked!');
+      });
+
+      this.account.mousePressed(() => {
+        console.log('Account clicked!');
+      });
+  }
+
+  setup() {
+      this.p5.createCanvas(390, 844);
+  }
+
+  draw() {
+      this.p5.background(220);
+
+      this.p5.image(this.back, 0, 0);
+      this.p5.image(this.logo, 90, 20);
+
+      this.p5.fill(255);
+      this.p5.rect(0, 200, 390, 650); // Recuadro blanco
+
+      this.p5.fill('#21272A');
+      this.p5.textSize(32);
+      this.p5.textAlign(this.p5.CENTER, this.p5.CENTER);
+      this.p5.textStyle(this.p5.BOLD)
   
-//       // Texto principal
-//       this.p5.text("Sign Up", 80, 260);
+      // Texto principal
+      this.p5.text("Sign Up", 80, 260);
   
-//       // Subtítulos
-//       this.p5.textSize(14);
-//       this.p5.text("First Name", 60, 320);
-//       this.p5.text("Last Name", 250, 320);
-//       this.p5.text("Email", 45, 420);
-//       this.p5.text("Password", 58, 525);
-//       this.p5.text("Gender", 50, 640);
+      // Subtítulos
+      this.p5.textSize(14);
+      this.p5.textStyle(this.p5.NORMAL)
+      this.p5.text("First Name", 60, 320);
+      this.p5.text("Last Name", 250, 320);
+      this.p5.text("Email", 45, 420);
+      this.p5.text("Password", 58, 525);
+      this.p5.text("Gender", 50, 640);
 
-//       this.p5.textSize(16);
+      this.p5.textSize(16);
 
-//       // Mostrar campos de entrada y botón
-//       this.firstNameInput.show();
-//       this.lastNameInput.show();
-//       this.emailInput.show();
-//       this.passwordInput.show();
-//       this.genderSelect.show();
-//       this.signUpButton.show();
-//   }
-// }
+      // Mostrar campos de entrada y botón
+      this.firstNameInput.show();
+      this.lastNameInput.show();
+      this.emailInput.show();
+      this.passwordInput.show();
+      this.genderSelect.show();
+      this.signUpButton.show();
+
+      // Dibujar la línea divisoria
+      this.p5.line(20, 790, 370, 790);
+  }
+}
