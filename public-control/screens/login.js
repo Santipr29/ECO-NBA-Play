26cd5b475c68f8970fbc14e87d91fd43db4e5e01
Santiago Perez <santipr2004@gmail.com>
2023-11-19 22:19:05 -0500

    feat: login screen style complete

--- public-control/screens/login.js
+++ public-control/screens/login.js
@@ -8,16 +8,16 @@ export class CellPhoneLogInScreen {
         this.socket = io.connect('http://localhost:5500', { path: '/real-time' });
     
         // Crear campos de entrada y botón usando createInput y createButton
-        this.emailInput = p5.createInput().position(24, 440).size(345, 30).attribute('type', 'email');
+        this.emailInput = p5.createInput().position(24, 350).size(332, 30).attribute('type', 'email');
         this.emailInput.style('background-color', '#F2F4F8').style('border-radius', '10px').style('border', '2px solid #C1C7CD').style('font-size', '16px').style('color', '#697077').style('font-weight', 'regular');
   
-        this.passwordInput = p5.createInput().position(24, 547).size(345, 30).attribute('type', 'password');
+        this.passwordInput = p5.createInput().position(24, 460).size(332, 30).attribute('type', 'password');
         this.passwordInput.style('background-color', '#F2F4F8').style('border-radius', '10px').style('border', '2px solid #C1C7CD').style('font-size', '16px').style('color', '#697077').style('font-weight', 'regular');
   
-        this.logInButton = p5.createButton('Log In').position(20, 720).size(350, 40);
+        this.logInButton = p5.createButton('Log In').position(20, 580).size(350, 40);
         this.logInButton.style('background-color', '#006AB7').style('border-radius', '10px').style('border', '2px solid #0F62FE').style('font-size', '16px').style('color', 'white').style('font-weight', 'bold');
   
-        this.account = p5.createButton('No account yet? Sign Up').position(19, 800);
+        this.account = p5.createButton('No account yet? Sign Up').position(19, 720);
         this.account.style('background-color', 'transparent').style('border', '1px solid transparent').style('font-size', '14px').style('color', '#001D6C').style('font-weight', 'regular');
   
         // Cargar imágenes
@@ -57,8 +57,8 @@ export class CellPhoneLogInScreen {
         // Subtítulos
         this.p5.textSize(14);
         this.p5.textStyle(this.p5.NORMAL)
-        this.p5.text("Email", 45, 420);
-        this.p5.text("Password", 58, 525);
+        this.p5.text("Email", 45, 330);
+        this.p5.text("Password", 58, 440);
   
         this.p5.textSize(16);
   
@@ -69,6 +69,6 @@ export class CellPhoneLogInScreen {
         this.account.show()
   
         // Dibujar la línea divisoria
-        this.p5.line(20, 790, 370, 790);
+        this.p5.line(20, 670, 370, 670);
     }
   }
