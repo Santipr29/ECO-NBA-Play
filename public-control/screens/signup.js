6181d40a4d41eaa3a5117c913b351515d8a7e970
Santiago Perez <santipr2004@gmail.com>
2023-11-19 23:02:53 -0500

    Feat: Sign Up corrections

--- public-control/screens/signup.js
+++ public-control/screens/signup.js
@@ -8,25 +8,25 @@ export class CellPhoneSignUpScreen {
       this.socket = io.connect('http://localhost:5500', { path: '/real-time' });
   
       // Crear campos de entrada y botón usando createInput y createButton
-      this.firstNameInput = p5.createInput().position(20, 340).size(150, 30);
+      this.firstNameInput = p5.createInput().position(20, 320).size(150, 30);
       this.firstNameInput.style('background-color', '#F2F4F8').style('border-radius', '10px').style('border', '2px solid #C1C7CD').style('font-size', '16px').style('color', '#697077').style('font-weight', 'regular');
 
-      this.lastNameInput = p5.createInput().position(213, 340).size(150, 30);
+      this.lastNameInput = p5.createInput().position(213, 320).size(150, 30);
       this.lastNameInput.style('background-color', '#F2F4F8').style('border-radius', '10px').style('border', '2px solid #C1C7CD').style('font-size', '16px').style('color', '#697077').style('font-weight', 'regular');
 
-      this.emailInput = p5.createInput().position(20, 440).size(345, 30).attribute('type', 'email');
+      this.emailInput = p5.createInput().position(20, 420).size(345, 30).attribute('type', 'email');
       this.emailInput.style('background-color', '#F2F4F8').style('border-radius', '10px').style('border', '2px solid #C1C7CD').style('font-size', '16px').style('color', '#697077').style('font-weight', 'regular');
 
-      this.passwordInput = p5.createInput().position(20, 540).size(345, 30).attribute('type', 'password');
+      this.passwordInput = p5.createInput().position(20, 520).size(345, 30).attribute('type', 'password');
       this.passwordInput.style('background-color', '#F2F4F8').style('border-radius', '10px').style('border', '2px solid #C1C7CD').style('font-size', '16px').style('color', '#697077').style('font-weight', 'regular');
 
-      this.genderSelect = p5.createSelect().position(20, 640).size(350, 40);
+      this.genderSelect = p5.createSelect().position(20, 620).size(350, 40);
       this.genderSelect.style('background-color', '#F2F4F8').style('border-radius', '10px').style('border', '2px solid #C1C7CD').style('font-size', '16px').style('color', '#697077').style('font-weight', 'regular');
 
-      this.signUpButton = p5.createButton('Sign Up').position(20, 720).size(350, 40);
+      this.signUpButton = p5.createButton('Sign Up').position(20, 700).size(350, 40);
       this.signUpButton.style('background-color', '#D71E54').style('border-radius', '10px').style('border', '2px solid #F43F74').style('font-size', '16px').style('color', 'white').style('font-weight', 'bold');
 
-      this.account = p5.createButton('Already have an account?').position(19, 800);
+      this.account = p5.createButton('Already have an account?').position(19, 780);
       this.account.style('background-color', 'transparent').style('border', '1px solid transparent').style('font-size', '14px').style('color', '#001D6C').style('font-weight', 'regular');
 
       // Agregar opciones al campo de selección
@@ -66,16 +66,16 @@ export class CellPhoneSignUpScreen {
       this.p5.textStyle(this.p5.BOLD)
   
       // Texto principal
-      this.p5.text("Sign Up", 80, 260);
+      this.p5.text("Sign Up", 80, 250);
   
       // Subtítulos
       this.p5.textSize(14);
       this.p5.textStyle(this.p5.NORMAL)
-      this.p5.text("First Name", 55, 320);
-      this.p5.text("Last Name", 248, 320);
-      this.p5.text("Email", 38, 420);
-      this.p5.text("Password", 52, 520);
-      this.p5.text("Gender", 45, 620);
+      this.p5.text("First Name", 55, 300);
+      this.p5.text("Last Name", 248, 300);
+      this.p5.text("Email", 38, 400);
+      this.p5.text("Password", 52, 500);
+      this.p5.text("Gender", 45, 600);
 
       this.p5.textSize(16);
 
@@ -89,6 +89,6 @@ export class CellPhoneSignUpScreen {
       this.account.show()
 
       // Dibujar la línea divisoria
-      this.p5.line(20, 790, 370, 790);
+      this.p5.line(20, 770, 370, 770);
   }
 }
